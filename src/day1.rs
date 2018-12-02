414fc9b8062babb96e92a049a17129e7084dea6d
Xavier Filippi <gueltir@gmail.com>
2018-12-02 01:32:07 +0100

    Add solution for second star

--- src/day1.rs
+++ src/day1.rs
@@ -2,8 +2,26 @@ use std::error::Error;
 use std::fs;
 use std::path::Path;
 
-fn main() -> Result<(), Box<Error + 'static>> {
+fn firstStar() -> Result<(), Box<Error + 'static>> {
     // Well that was painful: the file contain space to trim before parsing along with a final empty character, hence the unwrap_or
     println!("{}", fs::read_to_string(Path::new("./data/day1.txt"))?.split('\n').map( |x| x.trim().parse::<i32>().unwrap_or(0)).fold(0, |acc, x| x + acc));
+    Ok(())
+}
+
+fn secondStar() -> Result<(), Box<Error: 'static>> {
+    let mut found_frequency = HashSet::new();
+    found_frequency.insert(0);
+    let input = fs::read_to_string(Path::new("./data/day1.txt"))?.trim().split('\n').map( |x| x.trim().parse::<i32>().unwrap_or(0)).collect::<Vec<i32>>();
+    let mut acc = 0;
+
+    for frequency in input.iter().cycle() {
+        acc += frequency;
+        if !found_frequency.insert(acc) {
+            break;
+        }
+    }
+
+    println!("{}", acc);
+
     Ok(())
 }
