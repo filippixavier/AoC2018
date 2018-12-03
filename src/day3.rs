90a1fd6e851206108ea1f1dbd8a2c542d2205a8f
Xavier FILIPPI <xavier.filippi.prestataire@sfr.com>
2018-12-03 16:20:00 +0100

    Add 3rd day 2nd star

--- src/day3.rs
+++ src/day3.rs
@@ -5,13 +5,13 @@ use std::path::Path;
 
 use regex::Regex;
 
-fn main() -> Result<(), Box<Error + 'static>>{
+fn firstStar() -> Result<(), Box<Error + 'static>>{
     let input = fs::read_to_string(Path::new("./data/day3.txt"))?;
     // The starting r negate the need to escape special char in a string
-    // Also: no global flag, so we need to create a more complete regex
     let re = Regex::new(r"(?P<id>\d+) @ (?P<left>\d+),(?P<top>\d+): (?P<width>\d+)x(?P<height>\d+)").unwrap();
     let mut fabric: Vec<Vec<i32>> = vec![vec![0;1000];1000];
     let mut answer = 0;
+    // The iterator act as a defacto g flag
     for values in re.captures_iter(&input) {
         let left = values["left"].parse::<usize>().unwrap();
         let width = values["width"].parse::<usize>().unwrap();
@@ -28,6 +28,38 @@ fn main() -> Result<(), Box<Error + 'static>>{
     }
 
     println!("Overlapping elements are {}", answer);
+
+    Ok(())
+}
+
+// Could most probably do both star in on code, and/or use HashMap/HashSet more creatively
+fn secondStar() -> Result<(), Box<Error + 'static>>{
+    let input = fs::read_to_string(Path::new("./data/day3.txt"))?;
+    let re = Regex::new(r"(?P<id>\d+) @ (?P<left>\d+),(?P<top>\d+): (?P<width>\d+)x(?P<height>\d+)").unwrap();
+    let mut fabric: Vec<Vec<i32>> = vec![vec![0;1000];1000];
+    let mut answer = HashSet::new();
+    for values in re.captures_iter(&input) {
+        let mut overlap = false;
+        let left = values["left"].parse::<usize>().unwrap();
+        let width = values["width"].parse::<usize>().unwrap();
+        let id = values["id"].parse::<i32>().unwrap();
+        for i in left..left + width {
+            let top = values["top"].parse::<usize>().unwrap();
+            let height = values["height"].parse::<usize>().unwrap();
+            for j in top..top + height {
+                if fabric[i][j] != 0 {
+                    overlap = true;
+                    answer.remove(&fabric[i][j]);
+                }
+                fabric[i][j] = id;
+            }
+        }
+        if !overlap {
+            answer.insert(id);
+        }
+    }
+
+    println!("Non overlapping IDs are: {:?}", answer);
 
     Ok(())
 }
