bb323d5d0094325a1ead5c4dccebb045cf805928
Xavier Filippi <gueltir@gmail.com>
2018-12-05 00:28:43 +0100

    Change main.rs to launch correct solution depending on build features

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+// Note: look into build.rs to just rewrite mod day1 into mod dayx without all the cfg
+
+#[cfg(all(feature = "day1", not(feature = "nday")))]
+mod day1;
+#[cfg(feature = "day2")]
+mod day2;
+#[cfg(feature = "day3")]
+mod day3;
+#[cfg(feature = "day4")]
+mod day4;
+
+#[cfg(all(feature = "day1", not(feature = "nday")))]
+use day1::*;
+#[cfg(feature = "day2")]
+use day2::*;
+#[cfg(feature = "day3")]
+use day3::*;
+#[cfg(feature = "day4")]
+use day4::*;
+
 fn main() {
-    println!("Hello, world!");
+    match first_star() {
+        Err(x) => { println!("Error: {:?}", x); }
+        _ => { println!("Succeed!"); }
+    }
+    match second_star() {
+        Err(x) => { println!("Error {:?}", x); }
+        _ => { println!("Surcceed!"); }
+    }
 }
