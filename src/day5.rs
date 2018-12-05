2cc3943b06d2952743fccd2f816a121f2f2df143
Xavier FILIPPI <xavier.filippi.prestataire@sfr.com>
2018-12-05 10:43:01 +0100

    Add day5 src file

---
+++ src/day5.rs
@@ -0,0 +1,9 @@
+use std::error::Error;
+
+pub fn first_star() -> Result<(), Box<Error + 'static>> {
+	unimplemented!()
+}
+
+pub fn second_star() -> Result<(), Box<Error + 'static>> {
+	unimplemented!()
+}
