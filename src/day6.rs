b26eda7400e718154c4d96a2e3f60291d5da502b
Xavier Filippi <gueltir@gmail.com>
2018-12-07 02:55:36 +0100

    Further failed attempts

--- src/day6.rs
+++ src/day6.rs
@@ -3,87 +3,188 @@ extern crate regex;
 use std::error::Error;
 use std::fs;
 use std::path::Path;
-use std::cmp::*;
 
 use std::collections::HashMap;
 
 use self::regex::Regex;
 
+#[derive(Debug)]
+struct Point {
+    x: i32,
+    y: i32,
+    upper_right: Option<(i32, i32)>,
+    upper_left: Option<(i32, i32)>,
+    lower_right: Option<(i32, i32)>,
+    lower_left: Option<(i32, i32)>,
+}
+
 pub fn first_star() -> Result<(), Box<Error + 'static>> {
     let input = fs::read_to_string(Path::new("./data/day6.txt"))?;
     let reg = Regex::new(r"(?P<x>\d+), (?P<y>\d+)").unwrap();
+
+    let mut points = HashMap::<(i32, i32), Point>::new();
+    let mut areas = HashMap::<(i32, i32), i32>::new();
+    let mut map = HashMap::<(i32, i32), String>::new();
     
     // Our map and a list of discovered location
-    let mut map = HashMap::<(i32, i32), (String, i32)>::new();
-    let mut ids = HashMap::<String, (i32, i32)>::new();
-    let mut area_h = HashMap::<String, i32>::new();
-    
-    //Iterating over input's capture match (one match = one input)
-    for cap in reg.captures_iter(input.as_str()) {
-        let new_pt_id = cap.get(0).unwrap().as_str();
-        let (x, y) = (cap.get(1).unwrap().as_str().parse::<i32>().unwrap(), cap.get(2).unwrap().as_str().parse::<i32>().unwrap());
-        let mut area = 1;
+    for capture in reg.captures_iter(&input) {
+        let (col, line) = (capture.get(1).unwrap().as_str().parse::<i32>().unwrap(), capture.get(2).unwrap().as_str().parse::<i32>().unwrap());
 
-        // Iterating over each already discovered points
-        for (other_id, (other_x, other_y)) in ids.iter() {
-            let (start_x, end_x) = (min(x, *other_x), max(x, *other_x));
-            let (start_y, end_y) = (min(y, *other_y), max(y, *other_y));
-            // Iterating over the area between a discovered point and the new point
-            for temp_x in start_x..end_x + 1 {
-                for temp_y in start_y..end_y + 1 {
-                    let manhattan_dist = (temp_x - x).abs() + (temp_y - y).abs();
-                    let other_manhattan_dist = (temp_x - *other_x).abs() + (temp_y - *other_y).abs();
+        let mut new_pt = Point{x: col, y: line, upper_right: None, upper_left: None, lower_right: None, lower_left: None};
 
-                    //Systematically recalculate manhattan distance
-                    let (temp_id, temp_dist) = match map.get(&(temp_x, temp_y)) {
-                        Some((claiming_id, claiming_dist)) => {
-                            if *claiming_dist > manhattan_dist {
-                                if let Some(previous_area) = area_h.get_mut(claiming_id) {
-                                    *previous_area -= 1;
-                                }
-                                area += 1;
-                                (new_pt_id.to_string(), manhattan_dist)
-                            } else if *claiming_dist == manhattan_dist {
-                                if let Some(previous_area) = area_h.get_mut(claiming_id) {
-                                    *previous_area -= 1;
-                                }
-                                let result_id = ".".to_string();
-                                (result_id, *claiming_dist)
-                            } else {
-                                (claiming_id.clone(), *claiming_dist)
-                            }
+        for pt in points.values_mut() {
+            new_pt.upper_left = if pt.x <= new_pt.x && pt.y <= new_pt.y {
+                match pt.lower_right {
+                    Some((proxy_x, proxy_y)) => {
+                        let man = (new_pt.x - pt.x).abs() + (new_pt.y - pt.y).abs();
+                        let proxy_man = (pt.x - proxy_x).abs() + (pt.y - proxy_y).abs();
+
+                        if man < proxy_man {
+                            pt.lower_right = Some((new_pt.x, new_pt.y));
                         }
-                        None => {
-                            if manhattan_dist > other_manhattan_dist {
-                                if let Some(previous_area) = area_h.get_mut(other_id) {
-                                    *previous_area += 1;
+
+                        match new_pt.upper_left {
+                            Some((prev_x, prev_y)) => {
+                                let prev_man = (new_pt.x - prev_x).abs() + (new_pt.y - prev_y).abs();
+                                if prev_man > man {
+                                    Some((pt.x, pt.y))
+                                } else {
+                                    new_pt.upper_left
                                 }
-                                (other_id.clone(), other_manhattan_dist)
-                            } else if manhattan_dist == other_manhattan_dist {
-                                (".".to_string(), manhattan_dist)
-                            } else {
-                                area += 1;
-                                (new_pt_id.to_string(), manhattan_dist)
                             }
+                            None => Some((pt.x, pt.y)),
                         }
-                    };
-                    map.insert((temp_x, temp_y), (temp_id, temp_dist));
+                    },
+                    None => {
+                        pt.lower_right = Some((new_pt.x, new_pt.y));
+                        Some((pt.x, pt.y))
+                    }
                 }
-            }
+            } else {
+                new_pt.upper_left
+            };
+            new_pt.upper_right = if pt.x >= new_pt.x && pt.y <= new_pt.y {
+                match pt.lower_left {
+                    Some((proxy_x, proxy_y)) => {
+                        let man = (new_pt.x - pt.x).abs() + (new_pt.y - pt.y).abs();
+                        let proxy_man = (pt.x - proxy_x).abs() + (pt.y - proxy_y).abs();
+
+                        if man < proxy_man {
+                            pt.lower_left = Some((new_pt.x, new_pt.y));
+                        }
+
+                        match new_pt.upper_right {
+                            Some((prev_x, prev_y)) => {
+                                let prev_man = (new_pt.x - prev_x).abs() + (new_pt.y - prev_y).abs();
+                                if prev_man > man {
+                                    Some((pt.x, pt.y))
+                                } else {
+                                    new_pt.upper_right
+                                }
+                            }
+                            None => Some((pt.x, pt.y)),
+                        }
+                    },
+                    None => {
+                        pt.lower_left = Some((new_pt.x, new_pt.y));
+                        Some((pt.x, pt.y))
+                    }
+                }
+            } else {
+                new_pt.upper_right
+            };
+            new_pt.lower_left = if pt.x <= new_pt.x && pt.y >= new_pt.y {
+                match pt.upper_right {
+                    Some((proxy_x, proxy_y)) => {
+                        let man = (new_pt.x - pt.x).abs() + (new_pt.y - pt.y).abs();
+                        let proxy_man = (pt.x - proxy_x).abs() + (pt.y - proxy_y).abs();
+                        if man < proxy_man {
+                            pt.upper_right = Some((new_pt.x, new_pt.y));
+                        }
+                        match new_pt.lower_left {
+                            Some((prev_x, prev_y)) => {
+                                let prev_man = (new_pt.x - prev_x).abs() + (new_pt.y - prev_y).abs();
+                                if prev_man > man {
+                                    Some((pt.x, pt.y))
+                                } else {
+                                    new_pt.lower_left
+                                }
+                            }
+                            None => Some((pt.x, pt.y)),
+                        }
+                    },
+                    None => {
+                        pt.upper_right = Some((new_pt.x, new_pt.y));
+                        Some((pt.x, pt.y))
+                    }
+                }
+            } else {
+                new_pt.lower_left
+            };
+            new_pt.lower_right = if pt.x >= new_pt.x && pt.y >= new_pt.y {
+                match pt.upper_left {
+                    Some((proxy_x, proxy_y)) => {
+                        let man = (new_pt.x - pt.x).abs() + (new_pt.y - pt.y).abs();
+                        let proxy_man = (pt.x - proxy_x).abs() + (pt.y - proxy_y).abs();
+
+                        if man < proxy_man {
+                            pt.upper_left = Some((new_pt.x, new_pt.y));
+                        }
+                        match new_pt.lower_right {
+                            Some((prev_x, prev_y)) => {
+                                let prev_man = (new_pt.x - prev_x).abs() + (new_pt.y - prev_y).abs();
+                                if prev_man > man {
+                                    Some((pt.x, pt.y))
+                                } else {
+                                    new_pt.lower_right
+                                }
+                            }
+                            None => Some((pt.x, pt.y)),
+                        }
+                    },
+                    None => {
+                        pt.upper_left = Some((new_pt.x, new_pt.y));
+                        Some((pt.x, pt.y))
+                    }
+                }
+            } else {
+                new_pt.lower_right
+            };
         }
-        ids.insert(new_pt_id.to_string(), (x, y));
-        area_h.insert(new_pt_id.to_string(), area);
-    }
-    let mut answer = 0;
 
-    for val in area_h.values() {
-        answer = max(answer, *val);
-    }
+        points.insert((new_pt.x, new_pt.y), new_pt);
+   }
 
-    println!("Biggest safe area: {}", answer);
+
+    println!("Biggest safe area: {}", 1);
 
     Ok(())
 }
+
+fn name(dir: &mut Option<(i32, i32)>, opposite: &mut Option<(i32, i32)>, pt_x: i32, pt_y: i32, new_x: i32, new_y: i32) -> Option<(i32, i32)> {
+    let (x, y) = match dir {
+        Some((x, y)) => (*x, *y),
+        None => (0, 0),
+    };
+
+    if dir.is_none() {
+        *opposite = Some((new_x, new_y));
+        Some((pt_x, pt_y))
+    } else {
+        
+    }
+
+    /*match dir {
+        Some((proxy_x, proxy_y)) => {
+            
+        },
+        None => {
+            *opposite = Some((new_x, new_y));
+            Some((pt_x, pt_y))
+        }
+    }*/
+}
+
 
 pub fn second_star() -> Result<(), Box<Error + 'static>> {
     Ok(())
