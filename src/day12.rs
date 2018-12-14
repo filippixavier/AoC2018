23cf2d8b31bcba5239303949fa7fbeea33d4c87c
Xavier FILIPPI <xavier.filippi.prestataire@sfr.com>
2018-12-14 13:21:02 +0100

    Twelth day second star!

--- src/day12.rs
+++ src/day12.rs
@@ -7,12 +7,11 @@ use std::fs;
 use std::path::Path;
 
 use std::collections::HashMap;
-use std::collections::VecDeque;
 
 fn prepare_input() -> (String, HashMap<String, char>) {
     let input = fs::read_to_string(Path::new("./data/day12.txt")).unwrap();
     let initial_state_reg = Regex::new(r"((?:#|\.)+)").unwrap();
-    let initial_state = if let Some(cap) = initial_state_reg.captures(&input){
+    let initial_state = if let Some(cap) = initial_state_reg.captures(&input) {
         cap[1].to_string()
     } else {
         String::from(".")
@@ -28,118 +27,61 @@ fn prepare_input() -> (String, HashMap<String, char>) {
     (initial_state, transformation)
 }
 
-fn prepare_binary_input() -> (VecDeque<i32>, Vec<i32>) {
-    let input = fs::read_to_string(Path::new("./data/day12.txt")).unwrap();
-    let initial_state_reg = Regex::new(r"((?:#|\.)+)").unwrap();
-    let initial_state = if let Some(cap) = initial_state_reg.captures(&input){
-        cap[1].to_string().chars().map(|x| {
-            if x == '#' {
-                return 1;
-            }
-            return 0;
-        }).collect::<VecDeque<i32>>()
-    } else {
-        VecDeque::<i32>::new()
-    };
-    let mut transformation = vec!(0;0b100000);
-    let transformation_reg = Regex::new(r"((?:\.|#){5}) => (\.|#)").unwrap();
-
-    for capture in transformation_reg.captures_iter(&input) {
-        let (key, value) = (usize::from_str_radix(&capture[1].chars().map(|x| {
-            if x == '#' {
-                return '1';
-            }
-            return '0';
-        }).collect::<String>(), 2).unwrap(), capture[2].chars().nth(0).unwrap());
-        
-        transformation[key] = if value == '#' {
-            1
-        } else {
-            0
-        };
-    }
-
-    (initial_state, transformation)
-}
-
-fn binary_pots_value(steps: u64) -> i32 {
-    let (mut state, transformation) = prepare_binary_input();
-    let mut start = 0;
-    for _ in 0..steps {
-        let mut key = 0b0;
-        
-        let begin = state.pop_front().unwrap();
-        state.push_front(begin);
-        let end = state.pop_back().unwrap();
-        state.push_back(end);
-        
-        if begin == 1 {
-            state.push_front(0);
-            state.push_front(0);
-            start -= 2;
-        }
-        if end == 1 {
-            state.push_back(0);
-            state.push_back(0);
-        }
-        
-        let mut temp = state.clone();
-        for index in 0..state.len() + 2 {
-            key = ((key << 1) + state.get(index).unwrap_or(&0)) & 0b11111;
-            if index as i32 - 2 >= 0 {
-                temp[index - 2] = transformation[key as usize];
-            }
-        }
-        state = temp;
-    }
-    println!("State after {} steps:\n{:?}", steps, state.iter().map(|x| {return if *x == 1 {'#'} else {'.'}}).collect::<String>());
-    state.iter().enumerate().fold(0, |acc, (index, value)| {
-        if *value == 1 {
-            return acc + index as i32 + start;
-        }
-        acc
-    })
-}
-
-fn pots_values(steps: u64) -> i32 {
+fn pots_values(steps: i64) -> i64 {
     let (mut state, transformation) = prepare_input();
-    let mut start = 0;
-    for _ in 0..steps {
-        if state.ends_with("#") {
+    let mut states = Vec::<String>::new();
+    let all_pots_reg = Regex::new(r"(#+\.*)+#").unwrap();
+    let mut start: i64 = 0;
+    for i in 0..steps {
+        if state.ends_with('#') {
             state.push_str("..");
         }
-        if state.starts_with("#") {
+        if state.starts_with('#') {
             state.insert_str(0, "..");
             start -= 2;
         }
         let mut new_state = String::new();
         let mut cloned_state = state.clone().chars().collect::<Vec<char>>();
-        for i in 0..state.chars().count() {
+        for j in 0..state.chars().count() {
             let mut key = String::new();
-            if i == 0 {
+            if j == 0 {
                 key.push_str("..");
-            } else if i == 1 {
+            } else if j == 1 {
                 key.push('.');
-                key.push(*cloned_state.get(i - 1).unwrap());
+                key.push(cloned_state[j - 1]);
             } else {
-                key.push(*cloned_state.get(i - 2).unwrap());
-                key.push(*cloned_state.get(i - 1).unwrap());
+                key.push(cloned_state[j - 2]);
+                key.push(cloned_state[j - 1]);
             }
-            key.push(*cloned_state.get(i).unwrap());
-            key.push(*cloned_state.get(i+1).unwrap_or(&'.'));
-            key.push(*cloned_state.get(i+2).unwrap_or(&'.'));
+            key.push(cloned_state[j]);
+            key.push(*cloned_state.get(j + 1).unwrap_or(&'.'));
+            key.push(*cloned_state.get(j + 2).unwrap_or(&'.'));
             new_state.push(*transformation.get(&key).unwrap_or(&'.'));
         }
         state = new_state;
+
+        if let Some(found) = all_pots_reg.find(&state.clone()) {
+            let capture = &state[found.start()..found.end()].to_string();
+            // Shortcut since in my case the input have a cycle of 1, shifting to the right
+            if states.iter().position(|x| *x == *capture).is_some() {
+                println!("{}, {}", steps, i);
+                start += steps - 1 - i; // -1 since the range is [0, steps[
+                break;
+            } else {
+                states.push(capture.to_string());
+            }
+        }
     }
-    println!("State after {} steps:\n{}", steps, state);
-    let answer = state.chars().enumerate().fold(0, |acc, (index, ch)| {
+    println!(
+        "State after {} steps:\n{}, start at: {}",
+        steps, state, start
+    );
+    state.chars().enumerate().fold(0, |acc, (index, ch)| {
         if ch == '#' {
-            return acc + (index as i32 + start);
+            return acc + (index as i64 + start);
         }
         acc
-    });
-    answer
+    })
 }
 
 pub fn first_star() -> Result<(), Box<Error + 'static>> {
@@ -149,7 +91,8 @@ pub fn first_star() -> Result<(), Box<Error + 'static>> {
 }
 
 pub fn second_star() -> Result<(), Box<Error + 'static>> {
-    let answer = binary_pots_value(50000000000);
+    let answer = pots_values(50_000_000_000);
+    // 3650000001849 too high
     println!("Pots values: {}", answer);
     Ok(())
 }
