d3b6cdd9a5801dff00e631c5813c9b668f68d8b6
Xavier FILIPPI <xavier.filippi.prestataire@sfr.com>
2018-12-14 18:04:17 +0100

    Begin work on day 13

--- src/day13.rs
+++ src/day13.rs
@@ -1,7 +1,59 @@
+use std::error::Error;
+use std::fs;
+use std::path::Path;
+
+#[derive(Debug)]
+struct Cart {
+    current_dir: (i32, i32),
+    next_rotation: Rotate
+}
+
+#[derive(Debug)]
+enum Rotate {
+    Left,
+    Straight,
+    Right
+}
+
+impl Iterator for Cart {
+    type Item = (i32, i32);
+    fn next(&mut self) -> Option<Self::Item> {
+        Some((1, 1))
+    }
+}
+
+impl Cart {
+    fn new(orientation: char) -> Self {
+        Cart{current_dir: (0, 1), next_rotation: Rotate::Left}
+    }
+}
+
+fn get_map() -> Vec<Vec<i32>> {
+    let input = fs::read_to_string(Path::new("./data/day12.txt")).unwrap();
+    let mut cart_id = 6;
+    input.split('\n').map(|line| {
+        line.chars().map(|chara| {
+            match chara {
+                '|' => 1,
+                '-' => 2,
+                '/' => 3,
+                '\\'=> 4,
+                '+' => 5,
+                '^' | 'v' | '<' | '>' => {
+                    cart_id += 1;
+                    cart_id - 1
+                },
+                _ => 0
+            }
+        }).collect::<Vec<_>>()
+    }).collect::<Vec<_>>()
+}
+
 pub fn first_star() -> Result<(), Box<Error + 'static>> {
-	Ok(())
+    get_map();
+    Ok(())
 }
 
 pub fn second_star() -> Result<(), Box<Error + 'static>> {
-	Ok(())
+    Ok(())
 }
