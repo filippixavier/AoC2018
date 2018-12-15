316ed27cbcc88fe92710ca08ec688bcbb4238e60
Xavier Filippi <gueltir@gmail.com>
2018-12-15 05:13:31 +0100

    Thirteenth day second star

--- src/day13.rs
+++ src/day13.rs
@@ -3,15 +3,16 @@ use std::fs;
 use std::path::Path;
 
 use std::collections::HashMap;
+use std::collections::HashSet;
 
-#[derive(Debug)]
+#[derive(Copy, Clone, Debug)]
 enum Turn {
     Left,
     Right,
     Straight,
 }
 
-#[derive(Debug)]
+#[derive(Copy, Clone, Debug)]
 enum Orientation {
     Up,
     Left,
@@ -19,12 +20,12 @@ enum Orientation {
     Right,
 }
 
+#[derive(Clone, Debug)]
 struct Cart {
     position: (usize, usize),
     direction: (i32, i32),
     cross_turn: Turn,
     orientation: Orientation,
-    tile: char,
 }
 
 impl Cart {
@@ -103,12 +104,17 @@ impl Iterator for Cart {
     }
 }
 
-fn get_map() -> (Vec<Cart>, HashMap<(usize, usize), char>) {
+fn get_map() -> (
+    Vec<Cart>,
+    HashMap<(usize, usize), char>,
+    HashMap<(usize, usize), char>,
+) {
     let input = fs::read_to_string(Path::new("./data/day13.txt")).unwrap();
     let mut carts = Vec::<Cart>::new();
     let mut rails = HashMap::<(usize, usize), char>::new();
+    let mut occupied_tiles = HashMap::<(usize, usize), char>::new();
 
-    let lines = input.trim().split('\n');
+    let lines = input.split('\n');
     for (line_number, line_content) in lines.enumerate() {
         for (char_index, char_value) in line_content.chars().enumerate() {
             match char_value {
@@ -129,42 +135,42 @@ fn get_map() -> (Vec<Cart>, HashMap<(usize, usize), char>) {
                 }
                 '<' => {
                     rails.insert((char_index, line_number), '*');
+                    occupied_tiles.insert((char_index, line_number), '-');
                     carts.push(Cart {
                         position: (char_index, line_number),
                         direction: (-1, 0),
                         cross_turn: Turn::Left,
                         orientation: Orientation::Left,
-                        tile: '-',
                     });
                 }
                 '^' => {
                     rails.insert((char_index, line_number), '*');
+                    occupied_tiles.insert((char_index, line_number), '|');
                     carts.push(Cart {
                         position: (char_index, line_number),
                         direction: (0, -1),
                         cross_turn: Turn::Left,
                         orientation: Orientation::Up,
-                        tile: '|',
                     });
                 }
                 '>' => {
                     rails.insert((char_index, line_number), '*');
+                    occupied_tiles.insert((char_index, line_number), '-');
                     carts.push(Cart {
                         position: (char_index, line_number),
                         direction: (1, 0),
                         cross_turn: Turn::Left,
                         orientation: Orientation::Right,
-                        tile: '-',
                     });
                 }
                 'v' => {
                     rails.insert((char_index, line_number), '*');
+                    occupied_tiles.insert((char_index, line_number), '|');
                     carts.push(Cart {
                         position: (char_index, line_number),
                         direction: (0, 1),
                         cross_turn: Turn::Left,
                         orientation: Orientation::Down,
-                        tile: '|',
                     });
                 }
                 _ => {}
@@ -172,11 +178,11 @@ fn get_map() -> (Vec<Cart>, HashMap<(usize, usize), char>) {
         }
     }
 
-    (carts, rails)
+    (carts, rails, occupied_tiles)
 }
 
 pub fn first_star() -> Result<(), Box<Error + 'static>> {
-    let (mut carts, mut rails) = get_map();
+    let (mut carts, mut rails, mut occupied_tiles) = get_map();
     let crash_zone;
 
     'collide: loop {
@@ -187,7 +193,11 @@ pub fn first_star() -> Result<(), Box<Error + 'static>> {
             cart_a.position.1.cmp(&cart_b.position.1)
         });
         for cart in carts.iter_mut() {
-            rails.insert(cart.position, cart.tile);
+            // Instead of using another HashMap, it should be possible to store removed tiles in a vector sorted in the same order than the carts
+            rails.insert(
+                cart.position,
+                occupied_tiles.remove(&cart.position).unwrap(),
+            );
             let new_pos = cart.next().unwrap();
             let tile = *rails.get(&new_pos).unwrap_or(&'*');
             match tile {
@@ -207,7 +217,7 @@ pub fn first_star() -> Result<(), Box<Error + 'static>> {
                 _ => {}
             }
             rails.insert(cart.position, '*');
-            cart.tile = tile;
+            occupied_tiles.insert(cart.position, tile);
         }
     }
     println!("Crash in area {}, {}", crash_zone.0, crash_zone.1);
@@ -215,5 +225,65 @@ pub fn first_star() -> Result<(), Box<Error + 'static>> {
 }
 
 pub fn second_star() -> Result<(), Box<Error + 'static>> {
+    let (mut carts, mut rails, mut occupied_tiles) = get_map();
+
+    while carts.len() > 1 {
+        carts.sort_by(|cart_a, cart_b| {
+            if cart_a.position.1 == cart_b.position.1 {
+                return cart_a.position.0.cmp(&cart_b.position.0);
+            }
+            cart_a.position.1.cmp(&cart_b.position.1)
+        });
+        let mut colliding_pos: HashSet<(usize, usize)> = HashSet::new();
+        for cart in carts.iter_mut() {
+            rails.insert(
+                cart.position,
+                occupied_tiles.remove(&cart.position).unwrap(),
+            );
+            if colliding_pos.contains(&cart.position) {
+                continue;
+            }
+            let new_pos = cart.next().unwrap();
+            let tile = *rails.get(&new_pos).unwrap_or(&'*');
+            match tile {
+                '*' => {
+                    colliding_pos.insert(new_pos);
+                    continue;
+                }
+                '+' => {
+                    cart.handle_crossroad();
+                }
+                '/' => {
+                    cart.handle_slash('/');
+                }
+                '\\' => {
+                    cart.handle_slash('\\');
+                }
+                _ => {}
+            }
+            rails.insert(cart.position, '*');
+            occupied_tiles.insert(cart.position, tile);
+        }
+        carts = carts
+            .iter()
+            .cloned()
+            .filter(|cart| {
+                if colliding_pos.contains(&cart.position) {
+                    if let Some(tile) = occupied_tiles.remove(&cart.position) {
+                        rails.insert(cart.position, tile);
+                    }
+                    return false;
+                }
+                true
+            })
+            .collect::<_>();
+    }
+    let cart_posi = if let Some(cart) = carts.pop() {
+        cart.position
+    } else {
+        (0, 0)
+    };
+
+    println!("Last cart standing: {}, {}", cart_posi.0, cart_posi.1);
     Ok(())
 }
