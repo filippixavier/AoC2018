c5e61ac8da65b5158526ab6f00a8861103ccb088
Xavier Filippi <gueltir@gmail.com>
2018-12-29 23:05:26 +0100

    Eighteenth day first star

--- src/day18.rs
+++ src/day18.rs
@@ -1,6 +1,168 @@
 use std::error::Error;
 
+use std::fs;
+use std::path::Path;
+
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+enum State {
+    Open,
+    Trees,
+    Lumberyard,
+}
+
+fn prepare_input() -> (Vec<State>, usize) {
+    let input = fs::read_to_string(Path::new("./data/day18.txt")).unwrap();
+    let mut result = Vec::new();
+    let mut count = 0;
+    let mut stop_counting = false;
+    for ch in input.chars() {
+        match ch {
+            '.' => result.push(State::Open),
+            '#' => result.push(State::Lumberyard),
+            '|' => result.push(State::Trees),
+            _ => {
+                stop_counting = true;
+            }
+        }
+        if !stop_counting {
+            count += 1;
+        }
+    }
+    (result, count)
+}
+
+fn tile_counting(map: &[State], matching_state: State, index: usize, line_size: usize) -> usize {
+    let mut count = 0;
+    // left
+    if index > 0 && index % line_size > 0 && (index - 1) % line_size == (index % line_size) - 1 {
+        if let Some(&tile) = map.get(index - 1) {
+            if tile == matching_state {
+                count += 1;
+            }
+        }
+    }
+    // up-left
+    if index > line_size
+        && index % line_size > 0
+        && (index - 1) % line_size == (index % line_size) - 1
+    {
+        if let Some(&tile) = map.get(index - line_size - 1) {
+            if tile == matching_state {
+                count += 1;
+            }
+        }
+    }
+    // up
+    if index >= line_size {
+        if let Some(&tile) = map.get(index - line_size) {
+            if tile == matching_state {
+                count += 1;
+            }
+        }
+    }
+    // up-right
+    if index >= line_size
+        && index % line_size < line_size - 1
+        && (index + 1) % line_size == (index % line_size) + 1
+    {
+        if let Some(&tile) = map.get(index - line_size + 1) {
+            if tile == matching_state {
+                count += 1;
+            }
+        }
+    }
+    // right
+    if (index + 1) % line_size == (index % line_size) + 1 {
+        if let Some(&tile) = map.get(index + 1) {
+            if tile == matching_state {
+                count += 1;
+            }
+        }
+        // down-right
+        if let Some(&tile) = map.get(index + line_size + 1) {
+            if tile == matching_state {
+                count += 1;
+            }
+        }
+    }
+
+    // down
+    if let Some(&tile) = map.get(index + line_size) {
+        if tile == matching_state {
+            count += 1;
+        }
+    }
+
+    // down-left
+    if index > 0 && index % line_size > 0 && (index - 1) % line_size == (index % line_size) - 1 {
+        if let Some(&tile) = map.get(index + line_size - 1) {
+            if tile == matching_state {
+                count += 1;
+            }
+        }
+    }
+    count
+}
+
+fn evolve(old_state: &[State], line_size: usize) -> Vec<State> {
+    use self::State::*;
+    let mut new_state = old_state.to_owned();
+
+    for (index, state) in new_state.iter_mut().enumerate() {
+        match state {
+            Open => {
+                let count = tile_counting(&old_state, Trees, index, line_size);
+                if count >= 3 {
+                    *state = Trees;
+                }
+            }
+            Trees => {
+                let count = tile_counting(&old_state, Lumberyard, index, line_size);
+                if count >= 3 {
+                    *state = Lumberyard;
+                }
+            }
+            Lumberyard => {
+                let (count, count_trees) = (
+                    tile_counting(&old_state, Lumberyard, index, line_size),
+                    tile_counting(&old_state, Trees, index, line_size),
+                );
+                if count_trees < 1 || count < 1 {
+                    *state = Open;
+                }
+            }
+        }
+    }
+
+    new_state
+}
+
+fn visualize(map: &[State], line_size: usize) {
+    let mut txt = "".to_string();
+    for (index, i) in map.iter().enumerate() {
+        let temp = match i {
+            State::Open => ".",
+            State::Lumberyard => "#",
+            State::Trees => "|",
+        };
+        txt.push_str(temp);
+        if index % line_size == line_size - 1 {
+            txt.push('\n');
+        }
+    }
+    println!("{}", txt);
+}
+
 pub fn first_star() -> Result<(), Box<Error + 'static>> {
+    let (mut map, line_size) = prepare_input();
+    for _ in 0..10 {
+        map = evolve(&map, line_size);
+    }
+    let wood = map.iter().filter(|&x| *x == State::Trees).count();
+    let lumberyard = map.iter().filter(|&x| *x == State::Lumberyard).count();
+
+    println!("First star result: {}", wood * lumberyard);
+
     Ok(())
 }
 
