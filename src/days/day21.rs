ecc6f778fe238cf4e2c5b77ff136200dafd7f4e3
Xavier Filippi <gueltir@gmail.com>
2019-01-03 01:06:41 +0100

    Attempt at rewriting day 21 input

--- src/days/day21.rs
+++ src/days/day21.rs
@@ -22,8 +22,51 @@ pub fn first_star() -> Result<(), Box<Error + 'static>> {
     Ok(())
 }
 
+fn exec(instructions: &Vec<Operation>, reg_i: usize, start: usize) -> usize {
+    let mut regs: [usize; 6] = [0; 6];
+    regs[0] = start;
+    let mut count = 0;
+    loop {
+        let i_pointer = regs[reg_i];
+        if let Some(operation) = instructions.get(i_pointer) {
+            (operation.fun)(&mut regs, operation.arg_1, operation.arg_2, operation.arg_3);
+            regs[reg_i] += 1;
+            count += 1;
+        } else {
+            break;
+        }
+    }
+    count
+}
+
 pub fn second_star() -> Result<(), Box<Error + 'static>> {
-    // 1149790  too low
-    // 16777215 too high (MAX)
+    let a: u64 = 1;
+    let mut d: u64 = 10504829;
+    let mut e: u64;
+    let mut f: u64;
+    'main: loop {
+        e = d | 65_536;
+        d = 10_649_702;
+        loop {
+            f = e & 255;
+            d = f + d;
+
+            d = d & 16_777_215;
+            d = d * 65_899;
+            d = d & 16_777_215;
+
+            if 256 > e {
+                if d == a {
+                    break 'main;
+                } else {
+                    continue 'main;
+                }
+            } else {
+                e = e / 256;
+            }
+        }
+    }
     Ok(())
 }
+// 1149790  too low
+// 16777215 too high (MAX)
