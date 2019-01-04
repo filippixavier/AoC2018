254c60486f1aa0bcc60fd8acebc719483b098469
Xavier FILIPPI <xavier.filippi.prestataire@sfr.com>
2019-01-04 14:39:12 +0100

    Twenty-second day, fist star

--- src/days/day22.rs
+++ src/days/day22.rs
@@ -6,6 +6,71 @@ use std::path::Path;
 use self::regex::Regex;
 
 use std::error::Error;
+use std::fs;
+use std::path::Path;
+
+#[derive(Debug)]
+enum Region {
+    Rocky,
+    Wet,
+    Narrow
+}
+
+fn prepare_input() -> [usize; 3] {
+    let input = fs::read_to_string(Path::new("./data/day22.txt")).unwrap();
+    let reg = Regex::new(r"\d+").unwrap();
+    let mut values = [0; 3];
+
+    for (index, cap) in reg.captures_iter(&input).enumerate() {
+        values[index] = cap[0].parse::<usize>().unwrap();
+    }
+
+    // values[1] += 1;
+    // values[2] += 1;
+
+    values
+}
+
+fn create_map(depth: usize, target: (usize, usize)) -> Vec<usize> {
+    let mut map = Vec::<usize>::new();
+    let mut erosion_map = Vec::<u64>::new();
+    let line_size = target.0 + 1;
+    let line_number = target.1 + 1;
+
+    for index in 0..line_size * line_number {
+        let (x, y) = (index % line_size, index / line_size);
+        let geo_index: u64 = match (x, y) {
+            (0, 0) => 0,
+            (pos_x, 0) => pos_x as u64 * 16807,
+            (0, pos_y) => pos_y as u64 * 48271,
+            (pos_x, pos_y) => erosion_map[pos_x - 1 + pos_y * line_size] * erosion_map[pos_x + (pos_y - 1) * line_size]
+        };
+        let erosion_level = (geo_index + depth as u64) % 20183;
+        erosion_map.push(erosion_level);
+        map.push(erosion_level as usize % 3);
+    }
+    *map.last_mut().unwrap() = 0;
+
+    map
+}
+
+
+fn visualize(map: &[usize], line_size: usize) {
+    let mut txt = "".to_string();
+    for (index, i) in map.iter().enumerate() {
+        let temp = match i {
+            0 => ".",
+            1 => "=",
+            2 => "|",
+            _ => unreachable!()
+        };
+        txt.push_str(temp);
+        if index % line_size == line_size - 1 {
+            txt.push('\n');
+        }
+    }
+    println!("{}", txt);
+}
 
 fn prepare_input() -> [usize;3] {
     let input = fs::read_to_string(Path::new("./data/day22.txt")).unwrap();
